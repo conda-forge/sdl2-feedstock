971130d4c995ffbd454a6ec26d85197ba145c029
Tobias Fischer <info@tobiasfischer.info>
2021-04-08 12:27:08 +1000

    osx-arm fixed (#21)
    * migration: OSXArm
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.9.0, and conda-forge-pinning 2021.04.03.05.20.29
    
    * Add gnuconfig
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    Co-authored-by: regro-cf-autotick-bot <36490558+regro-cf-autotick-bot@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-scripts
+
 # we have to do this because most build scripts assume that all sdl modules
 # are installed to the same prefix.
 sed -i -- "s|@prefix@|${PREFIX}|g" sdl2.pc.in 

807b601d7d8d69ef326cf57c5561aa901d1a54ca
Stephen Rhodes <sr99622@gmail.com>
2022-07-11 17:33:28 -0400

    add pulseaudio dependency to enable audio on linux, disable wayland

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,15 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-scripts
 sed -i -- "s|@prefix@|${PREFIX}|g" sdl2.pc.in 
 sed -i -- "s|@prefix@|${PREFIX}|g" sdl2-config.in
 
+export PULSEAUDIO_CFLAGS=${PREFIX}/include
+export PULSEAUDIO_LIBS=${PREFIX}/lib
+
 # Build SDL2
 ./autogen.sh
 if [ -z ${OSX_ARCH+x} ]; then
-  ./configure --prefix=${PREFIX};
+  ./configure --prefix=${PREFIX} --enable-video-wayland=no;
 else
-  ./configure --prefix=${PREFIX} --without-x LDFLAGS="-framework ForceFeedback";
+  ./configure --prefix=${PREFIX} --without-x LDFLAGS="-framework ForceFeedback" --enable-video-wayland=no;
 fi
 
 make install

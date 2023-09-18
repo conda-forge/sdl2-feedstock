5aac4cbc4248f57b1427ff062f29033bda86a5bf
H. Vetinari <h.vetinari@gmx.com>
2023-09-18 17:15:53 +1100

    `#undef` main macro that messes up things

--- recipe/cmake_test/test_me.c
+++ recipe/cmake_test/test_me.c
@@ -1,6 +1,9 @@
 // check that SDL_GetVersion picks up correct version
 
 #include <SDL2/SDL.h>
+// who names a macro "main"...? linker errors on windows otherwise, see
+// https://stackoverflow.com/a/6847847
+#undef main
 
 int main(int argc, char** argv) {
     SDL_version C; // Compiled
